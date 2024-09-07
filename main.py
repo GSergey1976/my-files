563aa6b56de309612435b1f7a8821fe5d76f33a1
GSergey1976 <gsergey1976@gmail.com>
2024-09-07 16:45:02 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, PyCharm')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Задание архива
+# 1st programs
+print(9**0.5*5)
+# 2nd program
+print(9.99>9.98 and 1000 != 1000.1)
+# 3rd programs
+print(1234//10%100 + 5678//10%100)
+# 4th programs
+print(int(13.42==42.13*100%100) or int(42.13==13.42*100%100))
+print(int(13.42==42.13*100%100 or 42.13==13.42*100%100))
+print(int(13.42)==42.13*100%100)
+print(int(42.13)==13.42*100%100)
