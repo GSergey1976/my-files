03c80d49af00dfdd8c779ecbf806105ca8b5efcd
ЗВЕРЬ <gsergey1976@gmail.com>
2024-09-16 00:53:47 +0300

    15/09

--- moduie_1_3.py
+++ moduie_1_3.py
@@ -3,8 +3,8 @@ name='Name: Sergey'
 print(name)
 age= 48
 print('Age:',age)
-age= 49
-print('New Age:' ,age)
-c='is_student'
-is_student='True'
+new_age= age+3
+print('New Age:' ,new_age)
+c=is_student
+is_student=True
 print('Is_Student:', is_student)
