635488cfd2d25bacb88181cc8427620f2bdbdfb7
GSergey1976 <gsergey1976@gmail.com>
2024-09-16 14:06:03 +0300

    Add files via upload

--- moduie_1_3.py
+++ moduie_1_3.py
@@ -3,8 +3,8 @@ name='Name: Sergey'
 print(name)
 age= 48
 print('Age:',age)
-new_age= age+3
-print('New Age:' ,new_age)
+age= age+3
+print('New Age:' ,age)
 is_student=True
 print('Is_Student:', is_student)
 print(type(is_student))
