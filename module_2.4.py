26736155bbe5291745ffbc03aefb3c84ff0a3223
GSergey1976 <gsergey1976@gmail.com>
2024-10-07 20:29:22 +0300

    Add files via upload

--- module_2.4.py
+++ module_2.4.py
@@ -1,5 +1,3 @@
-from module_1_4 import number_my_string
-
 numbers=[1,2,3,4,5,6,7,8,9,10,11,12,12,14,15]
 primes = []
 not_primes = []
@@ -16,10 +14,11 @@ for i in numbers:
         for j in range(2,i):
             if i % j == 0:
                 is_primes = False
-                break
+                continue
         if is_primes:
             primes.append(i)
         else:
             not_primes.append(i)
+is_primes=True
 print("Простые числа", primes)
-print("Не простые числа", is_primes)
+print("Не простые числа", not_primes)
